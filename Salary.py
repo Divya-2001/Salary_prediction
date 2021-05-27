7e89fdcf29a21702c63dd15a079fa177792a5e35
Divya-2001 <60807380+Divya-2001@users.noreply.github.com>
2021-05-27 16:28:53 +0530

    Update Salary.py

--- Salary.py
+++ Salary.py
@@ -1,9 +1,11 @@
 import pandas as pd
+import numpy
 dataset = pd.read_csv('salarydata.csv')
 X = dataset[['YearsExperience']]
 y = dataset['Salary']
 from sklearn.linear_model import LinearRegression
 model = LinearRegression()
 model.fit(X,y)
-import joblib
-joblib.dump(model, 'finalsalary.pk1')
+yrsExp = float(input("Enter years of Experience : "))
+prediction = model.predict([[yrsExp]])
+print("Your Salary would be : ", prediction)
